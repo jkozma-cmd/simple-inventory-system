ac50ccea5189e4cbe244f5a8f2a3c6fda38e7e7a
jkozma-cmd <jaydonkozma@gmail.com>
2026-05-21 16:00:07 -0700

    Update Main.java
    Added update option and exception handling

--- inventory-management-system/src/inventoryapp/Main.java
+++ inventory-management-system/src/inventoryapp/Main.java
@@ -14,36 +14,59 @@ public class Main {
 				// Calls method to print the menu for user display and input
 				printMenu();
 				while(!scnr.hasNextInt()) {
-					System.out.println("Please enter a number 1-4: ");
+					System.out.println("Please enter a number 1-5: ");
 					scnr.next();
 				}
 				choice = scnr.nextInt();
 				scnr.nextLine();
 				
 				// Implemented a while loop and switch structure in order to check correct user input for response
-				while (choice != 4) {
+				while (choice != 5) {
 					switch (choice) {
-					case 1:
-			
-						System.out.print("Enter the product ID: ");
-						String id = scnr.nextLine();
-						
-						System.out.print("Enter the product name: ");
-						String name = scnr.nextLine();
-						
-						System.out.print("Enter the product price: ");
-						double price = scnr.nextDouble();
-						scnr.nextLine();
-						// Creates a new product and stores it within the inventory ArrayList
-						System.out.print("Enter the product quantity: ");
-						int quantity = scnr.nextInt();
-						scnr.nextLine();
-						Product p = new Product (id, name, price, quantity);
-						inventory.add(p);
-						
-						System.out.println("\nYour entries have been recorded, please press enter to return to the menu.");
-						scnr.nextLine();
+					case 1: {
+
+						// try catch statement within a switch case for exception handling of user
+						// inputs
+						try {
+
+							System.out.print("Enter the product ID: ");
+							String id = scnr.nextLine();
+							if (id.isEmpty()) {
+
+								throw new IllegalArgumentException();
+
+							}
+
+							System.out.print("Enter the product name: ");
+							String name = scnr.nextLine();
+
+							if (name.isEmpty()) {
+
+								throw new IllegalArgumentException();
+
+							}
+
+							System.out.print("Enter the product price: ");
+							double price = Double.parseDouble(scnr.nextLine());
+
+							System.out.print("Enter the product quantity: ");
+							int quantity = Integer.parseInt(scnr.nextLine());
+
+							// Creates a new product and stores it within the inventory ArrayList
+							Product p = new Product (id, name, price, quantity);
+							inventory.add(p);
+
+							System.out.println("\n\nYour entries have been recorded!");
+
+						} catch (IllegalArgumentException e) {
+							System.out.println(
+									"\nPlease enter the correct values in your response. You are being returned to the menu.");
+
+						}
+					}
+
 						break;
+
 					case 2:
 						// Implemented product removal from ArrayList inventory based on user input ID
 						// If no ID is found then the user will receive a display message saying that there was no product with the entered ID found
@@ -85,6 +108,49 @@ public class Main {
 						}
 						
 						break;
+					case 4:
+						// User Updates a product of their choice
+						// If the user input ID update matches the existing product in inventory ArrayList, then product updates with new user input
+						System.out.print("Please enter the ID of the product you would like to update: ");
+						String updateId = scnr.nextLine();
+						
+						boolean updateFound = false;
+						
+						for(Product products: inventory) {
+							System.out.printf("%-10s %-15s $%-15.2f %-8d\n", products.getId(),products.getName(),products.getPrice(),products.getQuantity());
+								if(products.getId().equalsIgnoreCase(updateId)) {
+									System.out.print("Enter the new product ID: ");
+									String idUpdate = scnr.nextLine();
+									
+									System.out.print("Enter the new product name: ");
+									String nameUpdate = scnr.nextLine();
+									
+									System.out.print("Enter the new product price: ");
+									double priceUpdate = scnr.nextDouble();
+									scnr.nextLine();
+						
+									System.out.print("Enter the new product quantity: ");
+									int quantityUpdate = scnr.nextInt();
+									scnr.nextLine();
+									
+									products.setId(idUpdate);
+									products.setName(nameUpdate);
+									products.setPrice(priceUpdate);
+									products.setQuantity(quantityUpdate);
+									
+									updateFound = true;
+									System.out.println("\nProduct has been successfully updated. Please press enter to return to the menu.");
+									scnr.nextLine();
+									break;
+								}
+							}
+						if(updateFound != true) {
+							System.out.printf("Product ID %s was not found.%n", updateId);
+							System.out.println("\nPlease press enter to return to the menu.");
+							scnr.nextLine();
+							break;
+						}
+						
 					default:
 						System.out.println("Please enter a valid option");
 					
@@ -94,7 +160,7 @@ public class Main {
 					// Calls the print menu method again 
 					printMenu();
 					while(!scnr.hasNextInt()) {
-						System.out.println("Please enter a number 1-4: ");
+						System.out.println("Please enter a number 1-5: ");
 						scnr.next();
 					}
 					choice = scnr.nextInt();
@@ -116,11 +182,11 @@ public class Main {
 						+ "1) Add Product\n"
 						+ "2) Remove product\n"
 						+ "3) View products\n"
-						+ "4) Exit manager\n");
+						+ "4) Update product\n"
+						+ "5) Exit manager\n");
 				
 			}
 		
 		
 		
 	}
-
